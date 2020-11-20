3c53a500cf0383cc38d66f31257783f5ce7f7eb4
Luca Cavretti <cavretiluca@live.it>
2020-11-20 16:21:19 +0100

    Rendering of objects in the array has to be fixed

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const myApp = new Vue({
+    el: '#root',
+    data: {
+        myArr: [{
+                src: "img/avatar_1.jpg",
+                name: "Marco"
+            },
+            {
+                src: "img/avatar_2.jpg",
+                name: "Gabriele"
+            },
+            {
+                src: "img/avatar_3.jpg",
+                Name: "Matteo"
+            },
+            {
+                src: "img/avatar_6.jpg",
+                Name: "Jessico Calcetto"
+            }
+        ],
+    },
+    methods: {}
+})
